b203c71b51e8ae4b53f19c12598420ad5a1ad05d
nookcoder <gus5427@naver.com>
2024-01-22 14:01:24 +0900

    백준 : 10828

---
+++ java/bakejoon/12865/MainClass.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class MainClass {
+    public static void main(String[] args) {
+        int N, K;
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+    // 각 물건은 무게(W)와 가치(V)를 가진다.
+    // 최대 K 만큼의 무게만을 넣을 수 있고, V 만큼 즐길 수 있다.
+    // 최대한 즐거운 여행을 하기 위해 배낭에 넣을 수 있는 물건들의 가치의 최대값
+
+
+    // 고려 대상 무게랑 가치
+    // 무게가 최대를 고른다고 가치가 최대가 되는 것은 아님
+
+}
