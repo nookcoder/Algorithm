06825c40805f3df784b0f89f364dba3231fc5c7d
nookcoder <gus5427@naver.com>
2024-04-01 16:41:03 +0900

    백준 : 17404

--- java/bakejoon/17404/Main.java
+++ java/bakejoon/17404/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    // 2번까지 최소
+    // 1번에서 최소 -> 2번 최소
+    // 3번 최소
+    private static int N;
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
 }
